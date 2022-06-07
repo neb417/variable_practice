d5aa81fb07863182a2ddd989c6244f192f80a959
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-07 15:31:12 -0600

    Final push

--- variables.rb
+++ variables.rb
@@ -1,8 +1,16 @@
 line1 = "first string"
 line2 = "next bit of string"
+line2 # "next bit of string"
+line2 = "new next bit of string"
 num1 = 1
 num2 = 2
 float1 = 25.4
 float2 = 92.44
 bool1 = true
 bool2 = false
+line1 # "first string"
+line1 = "New first string"
+puts "line 1"
+puts line1
+puts "line2"
+puts line2
